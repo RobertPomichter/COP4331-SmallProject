49e4a6f359b8da6d473bbe323559c5cd66397f12
maxgomer <77853113+maxgomer@users.noreply.github.com>
2021-01-28 01:09:08 -0500

    Add comments to Code.js, start doRegister()

--- js/code.js
+++ js/code.js
@@ -1,55 +1,126 @@
-var urlBase = 'http://COP4331-3.com/LAMPAPI';
+/*
+	This is the main JavaScript code file, which facilitates the communication
+	between the HTML pages and the API.
+
+	JavaScript is a weakly-typed language, so any variable is created using var
+	and then assigning it information. The nature of that information defines
+	the variable type (so var a = 5; is an integer ... var b = "Text Here" is a string)
+*/
+
+var urlBase = 'http://angryredangler.com/LAMPAPI';
 var extension = 'php';
 
 var userId = 0;
 var firstName = "";
 var lastName = "";
 
+// Performs the Log In function using user information captured by index.html
+// and by calling the Login.php API endpoint. ONLY used in Login Page.
 function doLogin()
 {
-	userId = 0;
+	// initialization section
+	userId = 0;	// weird that this isn't ID, it's Id, possible bug source?
 	firstName = "";
 	lastName = "";
 	
-	var login = document.getElementById("loginName").value;
-	var password = document.getElementById("loginPassword").value;
-//	var hash = md5( password );
+	var login = document.getElementById("loginName").value;	// grab loginName from index.html
+	var password = document.getElementById("loginPassword").value;	// grab loginPassword from index.html
+//	var hash = md5( password );	// create hashed password using md5.js algorithm
 	
+	// assigns an empty login result message (stays empty if login is successful)
 	document.getElementById("loginResult").innerHTML = "";
 
+	// JSON package creation - hashed password version
 //	var jsonPayload = '{"login" : "' + login + '", "password" : "' + hash + '"}';
-	var jsonPayload = '{"login" : "' + login + '", "password" : "' + password + '"}';
-	var url = urlBase + '/Login.' + extension;
 
-	var xhr = new XMLHttpRequest();
+	// JSON package creation - user's inputted password version
+	var jsonPayload = '{"login" : "' + login + '", "password" : "' + password + '"}';
+
+	var url = urlBase + '/Login.' + extension;	// url shortcut variable made to Login.php
+
+	/*
+	XML is a a programming tool that uses a language very similar to HTML,
+	but is only for handling data storage and transport.
+	*/
+
+	// XMLHttpRequests are objects used to communicate with servers.
+	// You can access page data without needing to refresh the page,
+	// which avoids interrupting the user during their visit.
+	var xhr = new XMLHttpRequest();	// constructor to initialize a new XMLHttpRequest (AKA: XHR)
+
+	// Starts the communication request using the POST method, which indicates that data is
+	// going to be sent to the Login.php file located at the "url". The Boolean is to specify
+	// the use of synchronous or asynchronous methods.
 	xhr.open("POST", url, false);
+
+	/* Sets the parameters of the HTTP Request Header, which is a header section in the HTTP
+	   request that defines information about the data that is going to be handled. Here the
+	   header's media type (listed as "Content-type"), is being described. The media type is
+	   described as an application type with a subtype of JSON, and "charset=UTF-8" describes
+	   the set of characters to be used. Charset=UTF-8 is an ASCII alternative character set
+	   and is preferred in web page coding.
+	*/
 	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+
+	// errors can happen during XHR communication
 	try
 	{
-		xhr.send(jsonPayload);
+		xhr.send(jsonPayload);	// sends the communication request (with the JSON data)
 		
+		// Parse the JSON formatted response string, which has each data element identified
+		// by extensions ".id", ".firstName", ".lastName"
 		var jsonObject = JSON.parse( xhr.responseText );
 		
-		userId = jsonObject.id;
+		userId = jsonObject.id;	// extract userID from jsonObject
 		
-		if( userId < 1 )
+		if( userId < 1 )	// if userID is < 1, no account match in database was found
 		{
+			// assign error message to loginResult in index.html
 			document.getElementById("loginResult").innerHTML = "User/Password combination incorrect";
 			return;
 		}
 		
-		firstName = jsonObject.firstName;
-		lastName = jsonObject.lastName;
+		firstName = jsonObject.firstName;	// extract firstName from jsonObject
+		lastName = jsonObject.lastName;		// extract lastName from jsonObject
 
-		saveCookie();
+		saveCookie();	// stores firstName, lastName, userId
+						// cookie expires after 20 minutes
 	
-		window.location.href = "color.html";
+		// TODO: convert to contact manager
+		window.location.href = "color.html";	// redirect user to color.html page
 	}
 	catch(err)
 	{
+		// assign error message to loginResult in index.html
 		document.getElementById("loginResult").innerHTML = err.message;
 	}
 
+}
+
+// TODO: Performs the Register User function using user information captured by register.html
+// and by calling the Register.php API endpoint
+function doRegister()
+{
+	// copy and pasting doLogin() code to modify
+	// initialization section
+	userId = 0;
+	firstName = "";
+	lastName = "";
+	
+	var login = document.getElementById("loginName").value;	// grab loginName from register.html
+	var password = document.getElementById("loginPassword").value;	// grab loginPassword from register.html
+//	var hash = md5( password );	// create hashed password using md5.js algorithm
+	
+	// assigns a register result message TODO: When does this message happen? What should be in it?
+	document.getElementById("registerResult").innerHTML = "";
+
+	// JSON package creation - hashed password version
+//	var jsonPayload = '{"login" : "' + login + '", "password" : "' + hash + '"}';
+
+	// JSON package creation - user's inputted password version
+	var jsonPayload = '{"login" : "' + login + '", "password" : "' + password + '"}';
+
+	var url = urlBase + '/Register.' + extension;	// url shortcut variable made to Register.php
 }
 
 function saveCookie()
