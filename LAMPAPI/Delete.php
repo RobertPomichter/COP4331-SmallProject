3e3982dc49b69a0b58b23381de522244e9f99a93
Joshua <joshualoufek@users.noreply.github.com>
2021-01-28 18:55:40 -0500

    Update Delete.php
    Added ' around phone to fix bug

--- LAMPAPI/Delete.php
+++ LAMPAPI/Delete.php
@@ -12,15 +12,14 @@
 	} 
 	else
 	{
-		$sql = "delete from Contacts where UserID = " . $id . " and Phone = " . $phone . "";
+		$sql = "delete from Contacts where UserId = " .$id. " and Phone = '".$phone."'";
 		if( $result = $conn->query($sql) != TRUE )
 		{
 			returnWithError( $conn->error );
 		}
 		$conn->close();
 	}
-	
-	//perhaps need to add some sort of affirmation of a successful delete
+
 	returnWithError("");
 
 	function getRequestInfo()
@@ -39,16 +38,6 @@
 		$retValue = '{"error":"' . $err . '"}';
 		sendResultInfoAsJson( $retValue );
 	}
-
-
-	//Example insert and delete command:
-	// insert into Colors (Name,UserID) VALUES ('Teal',1);
-	// delete from Colors where Name = 'Teal';
-	// delete from Colors where Name = 'light pink' and UserID = 3;
-
-
-	// insert into Contacts (UserID, FirstName, LastName, Email, Phone) VALUES (1, 'Josh', 'Test', 'joshtest@test.com', '(123)-456-7890');
-	// delete from Contacts where UserID = 1 and Phone = '(123)-456-7890';
 ?>
 
 
