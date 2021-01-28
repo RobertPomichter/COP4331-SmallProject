a45ff4b6852c6b559d21b540e146e376ba8737aa
Cydni Turner <54746916+cydni@users.noreply.github.com>
2021-01-28 15:03:39 -0500

    Update Register.php
    updated Register.php to version currently on server

--- LAMPAPI/Register.php
+++ LAMPAPI/Register.php
@@ -1,47 +1,45 @@
-<?php
-  
-	$inData = getRequestInfo(); //request the following info: first name, last name, login, and password
+<?php  
+        $inData = getRequestInfo(); //request the following info: first name, last name, login, and password
 
-	
-	$firstName = $inData["firstName"];	//read in first name
-	$lastName = $inData["lastName"];	//read in last name
-	$login = $inData["login"];		//read in login
-	$password = $inData["password"];	//read in password
+        
+        $firstName = $inData["firstName"];      //read in first name
+        $lastName = $inData["lastName"];        //read in last name
+        $login = $inData["login"];              //read in login
+        $password = $inData["password"];        //read in password
 
-	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
-	if ($conn->connect_error) 
-	{
-		returnWithError( $conn->connect_error );
-	} 
-	else
-	{
-		//insert into Users instead of Colors
-		$sql = "insert into Users (firstName,lastName, login, password) VALUES ('" . $firstName . "','" . $lastName . "','"$login"','"$password"')";
-		if( $result = $conn->query($sql) != TRUE )
-		{
-			returnWithError( $conn->error );
-		}
-		$conn->close();
-	}
-	
-	returnWithError("");
-	
-	function getRequestInfo()
-	{
-		return json_decode(file_get_contents('php://input'), true);
-	}
+        $conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+        if ($conn->connect_error) 
+        {
+                returnWithError( $conn->connect_error );
+        } 
+        else
+        {
+                //insert into Users instead of Colors
+                $sql = "insert into Users (firstName,lastName, login, password) VALUES ('" . $firstName . "','" . $lastName . "','".$login."','>
+                if( $result = $conn->query($sql) != TRUE )
+                {
+                        returnWithError( $conn->error );
+                }
+                $conn->close();
+        }       
+        returnWithError("");
+        
+        function getRequestInfo()
+        {
+                return json_decode(file_get_contents('php://input'), true);
+        }
 
-	function sendResultInfoAsJson( $obj )
-	{
-		header('Content-type: application/json');
-		echo $obj;
-	}
-	
-	function returnWithError( $err )
-	{
-		$retValue = '{"error":"' . $err . '"}';
-		sendResultInfoAsJson( $retValue );
-	}
-	
+        function sendResultInfoAsJson( $obj )
+        {
+                header('Content-type: application/json');
+                echo $obj;
+        }
+        
+        function returnWithError( $err )
+        {
+                $retValue = '{"error":"' . $err . '"}';
+                sendResultInfoAsJson( $retValue );
+        }
+        
 
 ?>
