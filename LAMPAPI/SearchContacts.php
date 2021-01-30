a57b7d55057d63f316d98b850cc372af345249e6
Cydni Turner <54746916+cydni@users.noreply.github.com>
2021-01-29 19:52:12 -0500

    Upload SearchContacts.php

---
+++ LAMPAPI/SearchContacts.php
@@ -0,0 +1,61 @@
+<?php
+
+	$inData = getRequestInfo();
+	
+	$searchResults = "";
+	$searchCount = 0;
+
+	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+	if ($conn->connect_error) 
+	{
+		returnWithError( $conn->connect_error );
+	} 
+	else
+	{
+		$sql = "select firstName, lastName, Email, Phone from Contacts where firstName like '%" . $inData["firstName"] . "%' and lastName like '%" .$inData["lastName"] . "%' and UserID=" . $inData["userId"];
+		$result = $conn->query($sql);
+		if ($result->num_rows > 0)
+		{
+			while($row = $result->fetch_assoc())
+			{
+				if( $searchCount > 0 )
+				{
+					$searchResults .= ",";
+				}
+				$searchCount++;
+				$searchResults .= '"' . $row["firstName"] . '" , "' .$row["lastName"]. '" , "' . $row["Email"] . '" , "' . $row["Phone"] . '"';
+			}
+		}
+		else
+		{
+			returnWithError( "No Records Found" );
+		}
+		$conn->close();
+	}
+
+	returnWithInfo( $searchResults );
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+	function returnWithInfo( $searchResults )
+	{
+		$retValue = '{"results":[' . $searchResults . '],"error":""}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?>
