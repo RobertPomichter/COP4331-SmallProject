9434f66562672cc72811af9f83fec7f38e8a3278
Joshua <joshualoufek@users.noreply.github.com>
2021-02-05 14:02:01 -0500

    Take Two

--- js/code.js
+++ js/code.js
@@ -152,6 +152,7 @@ function doRegister()
 	try
 	{
 		xhr.send(jsonPayload);	// sends the communication request (with the JSON data)
+		
 				
 		//Empty field error messge
 		if(firstName === "" || lastName === "" || login === "" || password === ""){
@@ -160,16 +161,10 @@ function doRegister()
 		}
 
 		// TEST: Add a success message section
-        else{
+               else{
 			 document.getElementById("registerResult").innerHTML = "Registration Successful!";
 		}	
 	// TODO: Add an account already exists message section?
-		
-		//Success message
-		else{
-			
-			document.getElementById("registerResult").innerHTML = "Registration Successful!";
-		}
 		
 	}
 	// Error if registration communication fails?
