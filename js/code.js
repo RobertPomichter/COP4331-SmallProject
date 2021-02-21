6a9663107d37af9889f2ae780426956e2a80e0f5
Max Gomer <maximilliangomer@gmail.com>
2021-02-21 02:03:25 -0500

    Max-FE-Polish-Wave1
    Tons of new toys and edits, see Max's newest video for a brief overview. Contact Manager page still needs to be polished, but waiting on that for now until after table reword so i dont break or interfere with anything

--- js/code.js
+++ js/code.js
@@ -739,3 +739,32 @@ function doTestUserId()
 
 	alert("UserId = " + userId);
 }
+
+
+// Button Wave/Ripple Animation - "Bootstrap Material Design Buttons" by Shahen Algoo
+// Imported from https://codepen.io/bootpen/pen/WrWZRd
+"use strict";
+function ripplesEffect(e) {
+    var waves, d, x, y;
+    
+    if($(this).find('.waves').length === 0) {
+        $(this).prepend('<span class="waves"></span>');
+    }
+     
+    waves = $(this).find('.waves');
+    waves.removeClass('ripple');
+     
+    if(!waves.height() && !waves.width()) {
+        d = Math.max($(this).outerWidth(), $(this).outerHeight());
+        waves.css({height: d, width: d});
+    }
+    
+    x = e.pageX - $(this).offset().left - waves.width()/2;
+    y = e.pageY - $(this).offset().top - waves.height()/2;
+     
+    waves.css({top: y+'px', left: x+'px'}).addClass('ripple');
+};
+
+$(document).ready(function(){
+    $('.btn, .ripples').on('click', ripplesEffect);
+});
